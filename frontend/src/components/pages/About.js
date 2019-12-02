75e4695579991c9802ce17dfd518fb5a8ffaa98b
Tristen Spruill <Tspruil6@uncc.edu>
2019-12-02 10:34:04 -0500

    Added last bit to FAQ and about pages

--- frontend/src/components/pages/About.js
+++ frontend/src/components/pages/About.js
@@ -10,6 +10,11 @@ const useStyles = makeStyles(theme => ({
     root: {
       flexGrow: 1,
     },
+    root1: {
+        flexGrow: 1,
+        paddingLeft:'32px',
+        textAlign:'center'
+      },
     paper: {
       padding: theme.spacing(1),
       textAlign: 'center',
@@ -27,6 +32,24 @@ export default function About() {
     const classes = useStyles();
     return(
         <React.Fragment>
+            <div className={classes.root1}>
+                <Grid container>
+                    <Grid item xs={12}>
+                    
+                    <h4>Project Description</h4>
+                    <p>This webapp will take the number of MAC addresses 
+                    connected to each wireless access point and give a real time description 
+                    of how many people are in the general vicinity, giving users a activie 'Heatmap' of campus wifi activity
+                    . On the left hand navigation it can show how busy each academic building is by each acess point. In the hotspot tab, we
+                    give a list of all the current Buildings, their access points and how many times each one has been pinged.
+
+                </p>
+                    
+                   
+                    </Grid>
+                    </Grid>
+                
+            </div>
             <div className={classes.root}>
                 <h2>This project was built by the following developers:</h2>
             <Grid container spacing={3}>
--- frontend/src/components/pages/FAQ.js
+++ frontend/src/components/pages/FAQ.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import { Paper } from '@material-ui/core'
+
+
+
 
 export default function FAQ() {
     return (
         <React.Fragment>
             <h2 style={{color: "#00703c" }}> FAQ </h2>
+            <div>
+                <Paper style={{padding:'16px'}} elevation={6}>
+                <p style={{color: "#B3A369" }}>Home: The main page where the Heatmap of campus activity resides</p>
+                <p>Hotspot: List of the buildings that are most curruntly active and their Access Points</p>
+                <p style={{color: "#B3A369" }}>About: Description of project and list of people who worked on it</p>
+                <p>Side Navigation Bar: The highest pinged access points and thier associated building</p>
+                </Paper>
+            </div>
         </React.Fragment>
     )
 }
